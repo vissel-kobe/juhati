db8882a900e585f133b364009429747119df0d9b
tesinhong <tesin0921@gmail.com>
2019-08-19 12:33:19 +0000

    admin以外のルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'labels/index'
-  get 'labels/edit'
-  get 'genres/index'
-  get 'genres/edit'
-  get 'artists/index'
-  get 'artists/edit'
-  get 'sale_histories/index'
-  get 'sale_histories/user_history'
-  get 'reviews/edit'
-  get 'albums/new'
-  get 'albums/index'
-  get 'albums/show'
-  get 'albums/edit'
-  get 'admins/edit'
-  get 'admins/show'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+  root 'homes#top'
   devise_for :users
+  
+  resources:users, only:[:index, :show, :edit, :update]
+  resources:artists, only:[:index, :edit, :create, :update, :destroy]
+  resources:albums do
+    resources:reviews, only:[:edit, :create, :update, :destroy]
+    resource:favorites, only:[:create, :destroy]
+  end
+  resources:labels, only:[:index, :edit, :create, :update, :destroy]
+  resources:genres, only:[:index, :edit, :create, :update, :destroy]
+  resources:orders, only:[:create, :update, :destroy]
+  resources:sale_histories, only:[:index, :user_history, :create]
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

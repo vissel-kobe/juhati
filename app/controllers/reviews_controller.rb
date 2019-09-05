452633b3c421c379ce71eccdaf6473806f9077a9
om-hondaseiha <honda.seiha.infratop@gmail.com>
2019-09-05 18:01:43 +0900

    配送先機能追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   def edit
   end
-  
+
   def create
     album = Album.find(params[:album_id])
     review = current_user.reviews.new(review_params)

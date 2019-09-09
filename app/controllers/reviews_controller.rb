93b3f82310d2ef74289384cbc172a592c8d6a740
tesinhong <tesin0921@gmail.com>
2019-09-09 06:47:58 +0000

    review edit destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,30 +1,36 @@
 class ReviewsController < ApplicationController
+  
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
+  def ensure_correct_user
+    @review = Review.find_by(id: params[:id])
+    if current_user.id != @review.user_id
+      redirect_to album_path(@review.album_id)
+    end
+  end
 
   def edit
     @user = current_user
     @album = Album.find(params[:album_id])
-    @review = current_user.reviews.find(params[:id])
   end
 
   def create
     @album = Album.find(params[:album_id])
     review = current_user.reviews.new(review_params)
     review.album_id = @album.id
+    review.user_id = current_user.id
     review.save
-    render :json => review
-    redirect_to album_path(@album)
+    redirect_to album_path(@album.id)
   end
 
   def update
     @album = Album.find(params[:album_id])
-    @review = current_user.reviews.find_by(id: params[:id])
     @review.update(review_params)
     redirect_to album_path(@album)
   end
 
   def destroy
     @album = Album.find(params[:album_id])
-    @review = current_user.reviews.find(params[:id])
     @review.destroy
     redirect_to album_path(@album)
   end

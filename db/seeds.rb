7ab171ecc9888a409e057ca99fb705834298365d
ooshirokaito <ooshiro.kaito.infratop@gmail.com>
2019-11-10 11:15:30 +0000

    商品追加を修正しました

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Admin.create!(email:  "kanrisya@kanrisya",
-#              password:  "kanrisya",
-#              password_confirmation: "kanrisya"
-#              )
-
-User.create!(family_name: "kanri",
-			 first_name: "kanri",
-			 kana_family_name: "管理",
-			 kana_first_name: "かんり",
-			 postcode: "111-1111",
-			 address: "aaa",
-			 telephone: "1111111",
-			 deleted: "",
-			 email:  "kanrisya@kanrisya",
-			 password:  "kanrisya",
-			 password_confirmation: "kanrisya"
+Admin.create!(email:  "kanrisya@kanrisya",
+             password:  "kanrisya",
+             password_confirmation: "kanrisya"
              )
+
+# User.create!(family_name: "kanri",
+# 			 first_name: "kanri",
+# 			 kana_family_name: "管理",
+# 			 kana_first_name: "かんり",
+# 			 postcode: "111-1111",
+# 			 address: "aaa",
+# 			 telephone: "1111111",
+# 			 deleted: "",
+# 			 email:  "kanrisya@kanrisya",
+# 			 password:  "kanrisya",
+# 			 password_confirmation: "kanrisya"
+#              )
